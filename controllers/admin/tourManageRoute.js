60ad9bf0a3d23bc84c8af174e1fdbc6f396e7b6f
ayakase <wibu1892001@gmail.com>
2023-09-15 01:52:55 +0700

    fix thumbnail

--- controllers/admin/tourManageRoute.js
+++ controllers/admin/tourManageRoute.js
@@ -1,37 +1,55 @@
 const express = require('express');
 const router = express.Router();
 const cloudinary = require('../../cloudiraryConfig');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
 const multer = require('multer');
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        // Specify the destination folder
+        cb(null, 'thumbnails/');
+    },
+    filename: function (req, file, cb) {
+        // Use the original filename as the saved filename
+        cb(null, file.originalname);
+    },
+});
+const upload = multer({ storage: storage });
 const Tour = require('../../models/TourModel')
 router.post('/', upload.single('tourThumbnail'), (req, res) => {
     console.log(req.body);
     console.log(req.file);
-    Tour.create({
-        title: req.body.tourTitle,
-        schedule: req.body.tourSchedule,
-        tourcategory: req.body.tourCategory,
-        tourtype: req.body.tourType,
-        departure: req.body.tourFrom,
-        days: req.body.tourLength,
-        ishottour: req.body.isHot,
-        transportation: req.body.tourTransport,
-        adultprice: req.body.adultPrice,
-        youngprice: req.body.youngPrice,
-        childprice: req.body.childPrice,
-        special: req.body.tourSpecial,
-        bonus: req.body.tourBonus,
-        visa: req.body.tourVisa,
-        detail: req.body.tourDetail,
-        priceservice: req.body.tourPriceService,
-        guide: req.body.tourGuide,
-    })
-        .then(() => {
-            res.json("done");
-        }).catch((err) => {
-            console.error(err)
-        })
+    // cloudinary.uploader.upload(req.file, (error, result) => {
+    //     if (error) {
+    //         console.error(error);
+    //     } else {
+    //         console.log(result.secure_url); // This is the URL for the uploaded image
+    //     }
+    // });
+    // Tour.create({
+    //     title: req.body.tourTitle,
+    //     schedule: req.body.tourSchedule,
+    //     tourcategory: req.body.tourCategory,
+    //     tourtype: req.body.tourType,
+    //     departure: req.body.tourFrom,
+    //     days: req.body.tourLength,
+    //     ishottour: req.body.isHot,
+    //     transportation: req.body.tourTransport,
+    //     adultprice: req.body.adultPrice,
+    //     youngprice: req.body.youngPrice,
+    //     childprice: req.body.childPrice,
+    //     special: req.body.tourSpecial,
+    //     bonus: req.body.tourBonus,
+    //     visa: req.body.tourVisa,
+    //     detail: req.body.tourDetail,
+    //     priceservice: req.body.tourPriceService,
+    //     guide: req.body.tourGuide,
+    // })
+    //     .then(() => {
+    //         res.json("done");
+    //     }).catch((err) => {
+    //         console.error(err)
+    //     })
 
 });
 
