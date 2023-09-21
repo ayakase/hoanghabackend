6f724ee1ff7eec33872f12649ce70ccbe033ffe5
ayakase <wibu1892001@gmail.com>
2023-09-22 06:22:19 +0700

    add some route

--- controllers/client/slideRoute.js
+++ controllers/client/slideRoute.js
@@ -2,14 +2,12 @@ const express = require('express');
 const router = express.Router();
 const Tour = require('../../models/TourModel')
 router.get('/:category/:order/:page', (req, res) => {
-    console.log(req.params.category);
-    Tour.findAndCountAll({
+    Tour.findAll({
         where: { tourcategory: req.params.category },
         order: [["createdAt", req.params.order]],
         limit: 10,
-        offset: (req.params.page - 1) * 10
     }).then((result) => {
-        const { count, rows } = result;
+        console.log(result);
         res.send(result)
     }).catch((error) => {
         console.error(error);
