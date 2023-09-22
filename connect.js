5c1a5b9130ee5b8099f2d64460eb447f60b626a4
ayakase <wibu1892001@gmail.com>
2023-09-22 13:43:16 +0700

    add dataabse

--- connect.js
+++ connect.js
@@ -1,15 +1,15 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('dulich_hoangha', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
-});
-
-
-// const sequelize = new Sequelize('freedb_dulich_hoangha', 'freedb_ayakase', 'nWNhJtexPs6!3mn', {
-//     host: 'sql.freedb.tech',
+// const sequelize = new Sequelize('dulich_hoangha', 'root', '', {
+//     host: 'localhost',
 //     dialect: 'mysql',
 // });
+
+
+const sequelize = new Sequelize('sql12648187', 'sql12648187', 'IwHTtaCMPd', {
+    host: 'sql12.freemysqlhosting.net',
+    dialect: 'mysql',
+});
 
 module.exports = sequelize;
 
--- test.js
+++
@@ -1,8 +0,0 @@
-const Tour = require('./models/TourModel')
-Tour.create({
-    title: "Tour test",
-    days: 5,
-    schedule: " from a to b",
-    transportation: "vehicle",
-    price: 100
-})
