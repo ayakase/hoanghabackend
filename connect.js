ba23ef193d67f4f1c44f994d5b33c3283abfd48b
ayakase <wibu1892001@gmail.com>
2023-09-24 18:05:06 +0700

    add env

--- connect.js
+++ connect.js
@@ -1,22 +1,16 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
-
-// const sequelize = new Sequelize('hoangha_travel', 'root', '', {
-//     host: 'localhost',
-//     dialect: 'mysql',
-// });
-
-
-// const sequelize = new Sequelize('sql12648187', 'sql12648187', 'IwHTtaCMPd', {
-//     host: 'sql12.freemysqlhosting.net',
-//     dialect: 'mysql',
-// });
-
-
-const sequelize = new Sequelize('hoangha', 'admin', 'cNpa65Ly', {
-    host: 'mysql-146874-0.cloudclusters.net',
+const sequelize = new Sequelize(process.env.SQL_DATABASE, process.env.SQL_USER, process.env.SQL_PASSWORD, {
+    host: process.env.SQL_SERVER,
     dialect: 'mysql',
-    port: 18583,
+    port: ''
 });
+
+// const sequelize = new Sequelize('hoangha', 'admin', 'cNpa65Ly', {
+//     host: 'mysql-146874-0.cloudclusters.net',
+//     dialect: 'mysql',
+//     port: 18583,
+// });
 
 module.exports = sequelize;
 
--- sync/syncAll.js
+++ sync/syncAll.js
@@ -3,10 +3,12 @@ const Order = require('../models/OrderModel');
 const Category = require('../models/CategoryModel');
 const FooterForm = require('../models/FooterFormModel');
 
-Category.sync({ force: true })
-    .then(() => {
-        console.log('User table created successfully.');
-        Category.bulkCreate([
+async function syncModelsSequentially() {
+    try {
+        // Sync Category model
+        await Category.sync({ force: true });
+        console.log('Category table created');
+        await Category.bulkCreate([
             {
                 id: 1,
                 name: "Trung Quốc",
@@ -21,30 +23,68 @@ Category.sync({ force: true })
                 id: 3,
                 name: "Quốc tế",
                 note: "none"
-            }]
-        )
-    })
-    .catch((error) => {
-        console.error('Error creating User table:', error);
-    });
-Tour.sync({ force: true })
-    .then(() => {
-        console.log('User table created successfully.');
-    })
-    .catch((error) => {
-        console.error('Error creating User table:', error);
-    });
-Order.sync({ force: true })
-    .then(() => {
-        console.log('User table created successfully.');
-    })
-    .catch((error) => {
-        console.error('Error creating User table:', error);
-    });
-FooterForm.sync({ force: true })
-    .then(() => {
-        console.log('User table created successfully.');
-    })
-    .catch((error) => {
-        console.error('Error creating User table:', error);
-    });
+            }]),
+            console.log('Input categories');
+        // Sync Tour model
+        await Tour.sync({ force: true });
+        console.log('Tour table created');
+
+        // Sync Order model
+        await Order.sync({ force: true });
+        console.log('Order table created');
+
+        // Sync FooterForm model
+        await FooterForm.sync({ force: true });
+        console.log('FooterForm table created');
+    } catch (error) {
+        console.error('Error creating tables:', error);
+    }
+}
+
+syncModelsSequentially();
+
+// Category.sync({ force: true })
+//     .then(() => {
+//         console.log('User table created successfully.');
+//         Category.bulkCreate([
+//             {
+//                 id: 1,
+//                 name: "Trung Quốc",
+//                 note: "none"
+//             },
+//             {
+//                 id: 2,
+//                 name: "Trong nước",
+//                 note: "none"
+//             },
+//             {
+//                 id: 3,
+//                 name: "Quốc tế",
+//                 note: "none"
+//             }]
+//         )
+//     })
+//     .catch((error) => {
+//         console.error('Error creating User table:', error);
+//     });
+// Tour.sync({ force: true })
+//     .then(() => {
+//         console.log('User table created successfully.');
+//     })
+//     .catch((error) => {
+//         console.error('Error creating User table:', error);
+//     });
+// Order.sync({ force: true })
+//     .then(() => {
+//         console.log('User table created successfully.');
+//     })
+//     .catch((error) => {
+//         console.error('Error creating User table:', error);
+//     });
+// FooterForm.sync({ force: true })
+//     .then(() => {
+//         console.log('User table created successfully.');
+//     })
+//     .catch((error) => {
+//         console.error('Error creating User table:', error);
+//     });
