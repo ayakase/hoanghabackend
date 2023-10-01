ec9a7dce6392705429e30ebb5cd9a9239e4d4e95
ayakase <wibu1892001@gmail.com>
2023-10-01 09:49:49 +0700

    a

---
+++ models/PostModel.js
@@ -0,0 +1,23 @@
+const sequelize = require('../connect');
+const { DataTypes } = require('sequelize');
+const Category = require('./CategoryModel');
+const Tour = sequelize.define('Tour', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    title: {
+        type: DataTypes.STRING(255),
+    },
+    thumbnail: {
+        type: DataTypes.STRING(255),
+    },
+    content: {
+        type: DataTypes.STRING(255),
+    },
+    tour_link: {
+        type: DataTypes.STRING(255),
+    }
+})
+module.exports = Tour;
